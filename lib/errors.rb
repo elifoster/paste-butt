eb508b6e3ea08e1a8385ab845df148a12175c1d6
elifoster <elifosterwy@gmail.com>
2015-11-27 15:14:29 -0800

    :seedling: Got started on Beta API. Currently only includes beta errors

--- lib/errors.rb
+++ lib/errors.rb
@@ -23,5 +23,55 @@ class Pastee
         'Error code 4: The syntax highlighting language provided was invalid.'
       end
     end
+
+    class Beta
+      class MustUseBetaError < StandardError
+        def message
+          'You must be using the Beta API for this action.'
+        end
+      end
+
+      class UnauthorizedError < StandardError
+        def message
+          'The provided API key is incorrect.'
+        end
+      end
+
+      class RequiresUserApplication < StandardError
+        def message
+          'This resource requires a UserApplication.'
+        end
+      end
+
+      class InvalidMethod < StandardError
+        def message
+          'The endpoint was requested with an invalid method.'
+        end
+      end
+
+      class InvalidFormat < StandardError
+        def message
+          'The request was in a format other than XML or JSON.'
+        end
+      end
+
+      class TooManyRequests < StandardError
+        def message
+          'Too many pastes have been submitted with the given API key.'
+        end
+      end
+
+      class InternalServerError < StandardError
+        def message
+          'There was a problem with the Pastee server.'
+        end
+      end
+
+      class ServiceUnavailable < StandardError
+        def message
+          'The service is temporarily offline for maintenance.'
+        end
+      end
+    end
   end
 end
--- lib/pastee.rb
+++ lib/pastee.rb
@@ -6,9 +6,14 @@ class Pastee
   # Creates a new instance of Pastee.
   # @param api_key
   # @param use_ssl [Boolean] Whether to use a secure SSL connection.
-  def initialize(api_key, use_ssl = true)
+  def initialize(api_key, use_ssl = true, use_beta = false)
     @url = use_ssl ? 'https://paste.ee/' : 'http://paste.ee/'
+    # Note that in the future, when the API is out of beta, this URL will need
+    #   to be updated to api.paste.ee rather than api.beta.paste.ee
+    @url = use_beta ? 'https://api.beta.paste.ee' : @url
+    @use_beta = use_beta
     @client = HTTPClient.new
+    @client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE if @use_beta
     @key = api_key
   end
 
@@ -50,5 +55,9 @@ class Pastee
     when 'error_invalid_key' then fail Pastee::Errors::InvalidKeyError
     when 'error_invalid_language' then fail Pastee::Errors::InvalidLanguageError
     end
+  end
+
+  def check_beta
+    fail Pastee::Errors::Beta::MustUseBetaError unless @use_beta
   end
 end
